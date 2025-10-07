c1648a2301c7288fd0f61d1b0db27437487a91fe
William Millen <wvmillen@gmail.com>
2025-10-07 17:21:26 -0400

    small tweaks

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router'
 import './Logo.css'
 import mainLogo from '../../assets/images/brand-assets/main-logo.svg'
 
 function Logo() {
   return (
-    <div className="logo">
+    <Link to="/" className="logo">
       <img src={mainLogo} alt="Looped" className="logo-image" />
       <span className="logo-text">ooped</span>
-    </div>
+    </Link>
   )
 }
 
