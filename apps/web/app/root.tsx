e5810e6460495a3b641b983776b7b15d43a3bddf
William Millen <wvmillen@gmail.com>
2025-12-22 22:57:25 -0500

    test 2

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+// Toggle to serve a site-wide 404 without removing routes/content.
 const forceNotFound = true;
 
 export async function loader() {
