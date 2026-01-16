03e78d1bdc6b5d8a8a355ac095972d18fedc570b
William Millen <wvmillen@gmail.com>
2026-01-16 16:17:29 -0500

    updated looped footer

--- apps/web/app/components/Footer/Footer.tsx
+++ apps/web/app/components/Footer/Footer.tsx
@@ -152,7 +152,7 @@ export function Footer() {
         </div>
 
         <div className="mt-10 flex flex-col gap-3 border-t border-border pt-6 text-sm text-text-light sm:flex-row sm:items-center sm:justify-between sm:gap-4 md:gap-6">
-          <p className="text-left sm:text-left">© {currentYear} Looped, Inc. All rights reserved.</p>
+          <p className="text-left sm:text-left">© {currentYear} Looped Social All rights reserved.</p>
           <p className="text-left sm:text-right">iOS app only</p>
         </div>
       </div>
